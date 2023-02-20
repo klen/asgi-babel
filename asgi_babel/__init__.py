d2c3c7469e971b020966cac5372a3c47682fc30e
Kirill Klenov <horneds@gmail.com>
2023-02-20 23:20:41 +0700

    build(version): 0.8.0 -> 0.8.1

--- asgi_babel/__init__.py
+++ asgi_babel/__init__.py
@@ -10,7 +10,7 @@ from asgi_tools.types import TASGIApp, TASGIReceive, TASGIScope, TASGISend
 
 from babel import Locale, support
 
-__version__ = "0.8.0"
+__version__ = "0.8.1"
 __license__ = "MIT"
 
 
